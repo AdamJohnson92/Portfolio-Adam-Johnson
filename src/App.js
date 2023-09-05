a3dfb49de602fc3df6c3db628aed46120df4f361
Adam <adamgjohnson92@gmail.com>
2023-09-05 11:18:37 -0700

    Added early level styling of header and footer and color scheme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import PortfolioHeader from './components/Header'
+import PortfolioFooter from './components/Footer';
 
 function App() {
   return (
     <div className='portfolio-app'>
        <PortfolioHeader />
+       <PortfolioFooter />
     </div>
 
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,11 @@
 import React from "react";
+
+function PortfolioFooter(){
+    return(
+        <div className="footer">
+            <p>Adam Johnson © 2023</p>
+        </div>
+    )
+}
+
+export default PortfolioFooter;
