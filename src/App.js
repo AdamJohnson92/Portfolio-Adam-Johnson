02bb5b69d97edfa582db2f10d495628d3c19a00b
Adam <adamgjohnson92@gmail.com>
2023-09-05 10:40:07 -0700

    Initial commit with Header component rendering in App.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import PortfolioHeader from './components/Header'
+
+function App() {
+  return (
+    <div className='portfolio-app'>
+       <PortfolioHeader />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PortfolioHeader(){
+    return(
+        <div className="header">
+            <h1>Adam Johnson</h1>
+            <h2>A Web Development Portfolio</h2>
+        </div>
+    )
+}
+
+export default PortfolioHeader;
