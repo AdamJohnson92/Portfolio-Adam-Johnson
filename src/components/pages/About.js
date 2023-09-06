f1b8d86e983c5e942bad5dae259f9e971d53b04e
Adam <adamgjohnson92@gmail.com>
2023-09-06 16:45:40 -0700

    Single static portfolio item is now included on portfolio page. Will add state and logic to dynamically render from object class

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div>
       <h3>About Me</h3>
-      <img src={require('./profile-picture.jpg')}   ></img>
+      <img src={require('../../assets/profile-picture.jpg')}   ></img>
       <p>
       Hello! My name is Adam Johnson. After graduating from Arizona State University, I worked for several years as a professional musician, before I decided it was time to explore other career options. <br></br>
       <br></br>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,10 +1,17 @@
 export default function Portfolio() {
-    return (
-      <div>
-        <h3>Portfolio Page</h3>
-        <p>
-         Portfolio
-        </p>
+  return (
+    <div>
+      <h3>Portfolio</h3>
+      <div className="portfolio-container">
+        <div className="portfolio-item">
+          <a href="https://dolly-bleeter-6a4dd49c0ee0.herokuapp.com/"><img className="portfolio-item-img" src={require('../../assets/dolly.png')}></img></a>
+          <a className='portfolio-item-text-area'href='https://github.com/Tmollerhoj/Dolly'>
+            <h4>Dolly Extra Words</h4>
+            <p className='portfolio-item-desc'>A clone of the Social Media Platform Formerly Known As Twitter. Users can 'bleet' and comment on each other's bleets. </p></a>
+
+        </div>
+
       </div>
-    );
-  }
+    </div>
+  );
+}
