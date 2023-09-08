0556178984ee975b5ad4a2c6954cf0cc2c96f6f8
Adam <adamgjohnson92@gmail.com>
2023-09-08 16:30:04 -0700

    Added resume download link to resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,10 @@
+import resume from './resume.pdf'
+
 export default function Resume() {
-    return (
-      <div>
-        <h3>Resume</h3>
-        <p>
-         Resume
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h3>Resume</h3>
+      <a href={require("./resume.pdf" )}download={'Adam_G_Johnson_Resume.pdf'}> Click here to download the pdf</a>
+    </div>
+  );
+}
