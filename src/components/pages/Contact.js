e83f010bce4733a6976e0a09402e379cd10da4c6
Adam <adamgjohnson92@gmail.com>
2023-09-11 11:58:14 -0700

    Contact form clears after post-submit alert

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -9,14 +9,12 @@ export default function Contact() {
     const name = event.target.name;
     const value = event.target.value
     setInputs(values => ({ ...values, [name]: value }))
-
   }
 
   const handleSubmit = (event) => {
     const validEmailRegex = new RegExp(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/)
 
     const emailCheck = validEmailRegex.test(inputs.email)
-    console.log(emailCheck)
     if (emailCheck === false) {
       window.alert('Not a valid email address')
     }
@@ -24,10 +22,11 @@ export default function Contact() {
       window.alert('All three fields must be complete.')
     }
     event.preventDefault()
-    console.log(inputs.name)
+
 
     window.alert('Thank you for reaching out!')
-    
+    setInputs(inputs.name = '', inputs.email = '', inputs.message = '')
+
   }
 
   return (
@@ -36,13 +35,13 @@ export default function Contact() {
       <form className="contact-form" action="mailto:adamgjohnson92@gmail.com" method="POST" encType="text/plain">
         <a className="tool-tip">
           <label className='form-label' htmlFor='name'>Name:
-          <input className='form-box' type='text' name='name' value={inputs.name || ''} onChange={handleInputChange}></input><Tooltip anchorSelect=".tool-tip" place='top'>This is a required field.</Tooltip>
-         </label>
+            <input className='form-box' type='text' name='name' value={inputs.name || ''} onChange={handleInputChange}></input><Tooltip anchorSelect=".tool-tip" place='top'>This is a required field.</Tooltip>
+          </label>
         </a>
 
         <a className="tool-tip">
           <label className='form-label' htmlFor='email'>Email:
-          <input className='form-box' type='text' name='email' value={inputs.email || ''} onChange={handleInputChange}></input><Tooltip anchorSelect=".tool-tip" place='top'>This is a required field.</Tooltip>
+            <input className='form-box' type='text' name='email' value={inputs.email || ''} onChange={handleInputChange}></input><Tooltip anchorSelect=".tool-tip" place='top'>This is a required field.</Tooltip>
           </label>
         </a>
 
@@ -51,7 +50,8 @@ export default function Contact() {
           <textarea className='form-box' id='message-box' type='text' name='message' value={inputs.message || ''} onChange={handleInputChange}><Tooltip anchorSelect=".tool-tip" place='top'>This is a required field.</Tooltip></textarea>
         </a>
 
-        <input className='btn' type='submit' value='Send' onClick={handleSubmit} />
+        <input className='btn' type='submit' value='Send' onClick={handleSubmit}
+        />
 
 
       </form>
