d87c1f53aebff513629ceddc9af33886def30f94
Adam <adamgjohnson92@gmail.com>
2023-09-11 13:38:50 -0700

    Added more portfolio items, and expanded the content of the About Me page.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,9 +6,9 @@ export default function About() {
       <h3>About Me</h3>
       <img src={require('../../assets/profile-picture.jpg')}   ></img>
       <p className='text-left'>
-      Hello! My name is Adam Johnson. After graduating from Arizona State University, I worked for several years as a professional musician, before I decided it was time to explore other career options. <br></br>
+      Hello! My name is Adam Johnson. After graduating from Arizona State University with my Bachelor's degree in Psychology, I worked for several years as a professional musician, playing the drums all over the United States before I decided it was time to explore other career options. <br></br>
       <br></br>
-      I began my journey as a web developer in April of 2023, once again through Arizona State University. I instantly became hooked on the problem-solving that is so abundant in the field of web development, and I'm excited to continue on this journey and build my skills.
+      I began my journey as a web developer in April of 2023, once again through Arizona State University. I instantly became fascinated by the problem-solving that is so abundant in the field of web development. Gaining new skills was thrilling, and I became hooked on the feeling of solving a difficult coding problem. My enthusiam for web development continued throughout the course, and in October of 2023, I completed the FullStack Flex Bootcamp through ASU and EdX. I'm excited to continue to build my skills, learn knew things, and expand my horizons in an ever changing field. 
       </p>
     </div>
   );
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,8 +2,10 @@ import React from "react";
 import dolly from "../../assets/dolly.png"
 import maki from "../../assets/maki.png"
 import rpg from "../../assets/rpg.PNG"
+import tech from "../../assets/tech-blog.png"
+import rainy from "../../assets/rainy.png"
+import readme from "../../assets/readme-generator.png"
 import PortItem from "../PortItem.js";
-
 
 const portfolioRoster = [
   {
@@ -26,6 +28,22 @@ const portfolioRoster = [
   description: 'A turn-based fantasy RPG game where you play as a hero and slay monsters. Made entirely with vanilla javascript.',
   gitLink: 'https://github.com/AdamJohnson92/RPG',
   deployedLink: 'https://adamjohnson92.github.io/RPG/'
+},{
+  itemName: 'Rainy Day Parkz',
+  image: rainy,
+  description: "A weather app that allows users to search for a National Park and pull up the current and forecasted weather for that park.",
+  gitLink: 'https://github.com/AdamJohnson92/Rainy-Day-Flightz',
+  deployedLink: 'https://adamjohnson92.github.io/Rainy-Day-Flightz/'
+}, {
+itemName: 'MVC Tech Blog',
+image: tech,
+description: "A tech blog app built using MVC framework with Handlebars. Users can share thoughts on current technologies in web development.",
+gitLink:'https://github.com/AdamJohnson92/MVC-Tech-Blog'
+},{
+  itemName: 'Command Line README Generator',
+  image: readme,
+  description: "A very useful app that allows users to answer Inquirer prompts in a command-line application that will generate a README.md file.",
+  gitLink: 'https://github.com/AdamJohnson92/README_Generator'
 }
 ]
 
@@ -35,7 +53,8 @@ export default function Portfolio() {
   return (
     <div className='body-content'>
       <h3>Portfolio</h3>
-      <p>Click the text box to visit the GitHub Repository. Click the image to visit the deployed application.</p>
+      <p>Click the text box to visit the GitHub Repository. Click the image to visit the deployed application if it is still actively deployed.</p>
+      <p>(Note: Some applications are used entirely in the command line interface and have no deployments. Other applications that were deployed with Heroku may no longer have live deployments)</p>
       <div className="portfolio-container">
         <PortItem portfolioRoster={portfolioRoster} />
       </div>
