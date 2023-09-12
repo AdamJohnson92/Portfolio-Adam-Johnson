b3dab1dee2a6d4bc0ab25b553d94c28b82a9d603
Adam <adamgjohnson92@gmail.com>
2023-09-11 17:49:20 -0700

    Refactored Contact form useState hooks. It's a few more lines of code, but it is much clear how things are operating. Worth it.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,30 +3,48 @@ import { Tooltip } from "react-tooltip";
 
 export default function Contact() {
 
-  const [inputs, setInputs] = useState({})
+  const [nameForm, setNameForm] = useState('')
+  const [emailForm, setEmailForm] = useState('')
+  const [messageForm, setMessageForm] = useState('')
 
+  
   const handleInputChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value
-    setInputs(values => ({ ...values, [name]: value }))
+    const inputName = event.target.name;
+    const inputValue = event.target.value
+
+
+    if (inputName === 'name') {
+      setNameForm(inputValue)
+      console.log(inputValue)
+    } else if (inputName === 'email') {
+      setEmailForm(inputValue)
+      console.log(inputValue)
+    } else if (inputName === 'message') {
+      setMessageForm(inputValue)
+      console.log(inputValue)
+    }
   }
+   
+  
 
   const handleSubmit = (event) => {
     const validEmailRegex = new RegExp(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/)
 
-    const emailCheck = validEmailRegex.test(inputs.email)
+    const emailCheck = validEmailRegex.test(emailForm)
     if (emailCheck === false) {
       window.alert('Not a valid email address')
     }
-    if (!inputs.name || !inputs.email || !inputs.message) {
+    if (!nameForm || !emailForm || !messageForm) {
       window.alert('All three fields must be complete.')
     }
     event.preventDefault()
 
-
-    window.alert('Thank you for reaching out!')
-    setInputs(inputs.name = '', inputs.email = '', inputs.message = '')
-
+    if (emailCheck === true && nameForm && emailForm && messageForm) {
+       window.alert('Thank you for reaching out!')
+    setNameForm('')
+    setEmailForm('')
+    setMessageForm('')
+    }
   }
 
   return (
@@ -37,19 +55,19 @@ export default function Contact() {
       <form className="contact-form" action="mailto:adamgjohnson92@gmail.com" method="POST" encType="text/plain">
         <a className="tool-tip">
           <label className='form-label' htmlFor='name'>Name:
-            <input className='form-box' type='text' name='name' value={inputs.name || ''} onChange={handleInputChange}></input><Tooltip anchorSelect=".tool-tip" place='top'>This is a required field.</Tooltip>
+            <input className='form-box' type='text' name='name' value={nameForm || ''} onChange={handleInputChange}></input><Tooltip anchorSelect=".tool-tip" place='top'>This is a required field.</Tooltip>
           </label>
         </a>
 
         <a className="tool-tip">
           <label className='form-label' htmlFor='email'>Email:
-            <input className='form-box' type='text' name='email' value={inputs.email || ''} onChange={handleInputChange}></input><Tooltip anchorSelect=".tool-tip" place='top'>This is a required field.</Tooltip>
+            <input className='form-box' type='text' name='email' value={emailForm || ''} onChange={handleInputChange}></input><Tooltip anchorSelect=".tool-tip" place='top'>This is a required field.</Tooltip>
           </label>
         </a>
 
         <a className="tool-tip">
           <label className='form-label' htmlFor='message'>Message :
-          <textarea className='form-box' id='message-box' type='text' name='message' value={inputs.message || ''} onChange={handleInputChange}><Tooltip anchorSelect=".tool-tip" place='top'>This is a required field.</Tooltip></textarea></label>
+          <textarea className='form-box' id='message-box' type='text' name='message' value={messageForm || ''} onChange={handleInputChange}><Tooltip anchorSelect=".tool-tip" place='top'>This is a required field.</Tooltip></textarea></label>
           
         </a>
 
