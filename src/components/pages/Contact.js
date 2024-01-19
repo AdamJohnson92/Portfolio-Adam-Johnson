3707961cdb89ff82410d599814b9dcd5a8ec3281
AdamJohnson92 <127777953+AdamJohnson92@users.noreply.github.com>
2024-01-19 12:25:41 -0700

    Added github and linkedIn links to contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
   const [emailForm, setEmailForm] = useState('')
   const [messageForm, setMessageForm] = useState('')
 
-  
+
   const handleInputChange = (event) => {
     const inputName = event.target.name;
     const inputValue = event.target.value
@@ -24,8 +24,8 @@ export default function Contact() {
       console.log(inputValue)
     }
   }
-   
-  
+
+
 
   const handleSubmit = (event) => {
     const validEmailRegex = new RegExp(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/)
@@ -40,10 +40,10 @@ export default function Contact() {
     event.preventDefault()
 
     if (emailCheck === true && nameForm && emailForm && messageForm) {
-       window.alert('Thank you for reaching out!')
-    setNameForm('')
-    setEmailForm('')
-    setMessageForm('')
+      window.alert('Thank you for reaching out!')
+      setNameForm('')
+      setEmailForm('')
+      setMessageForm('')
     }
   }
 
@@ -52,6 +52,13 @@ export default function Contact() {
       <h3>Contact Adam</h3>
       <h2>Email: adamgjohnson92@gmail.com</h2>
       <h2>Phone: 480-421-8720</h2>
+      <h2>
+        <a className="footer-link" href="https://www.linkedin.com/in/adam-johnson-61769b21a/">LinkedIn</a>
+      </h2>
+      <h2>
+        <a className="footer-link" href="https://github.com/AdamJohnson92">GitHub</a>
+      </h2>
+
 
       {/* <form className="contact-form" action="mailto:adamgjohnson92@gmail.com" method="POST" encType="text/plain">
         <a className="tool-tip">
