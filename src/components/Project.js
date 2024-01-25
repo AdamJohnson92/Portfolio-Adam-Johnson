89180a3ae8becd9bb3593cd972e407d1f73ae60d
AdamJohnson92 <127777953+AdamJohnson92@users.noreply.github.com>
2024-01-25 11:12:09 -0700

    Included the alt tag in the function that maps each portfolio item to the portfolio page.

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ export default function PortItem( props ) {
     <>
       {props.portfolioRoster.map((item) => (
         <div className="portfolio-item">
-          <a href={item.deployedLink}><img className="portfolio-item-img" src={item.image}></img></a>
+          <a href={item.deployedLink}><img className="portfolio-item-img" src={item.image} alt={item.altTag}></img></a>
           <a className='portfolio-item-text-area' href={item.gitLink}>
             <h4>{item.itemName}</h4>
             <p className='portfolio-item-desc'> {item.description} </p></a>
