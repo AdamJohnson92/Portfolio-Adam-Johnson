f86ec15d182d61fa2ee515a7621ea343b9054358
AdamJohnson92 <127777953+AdamJohnson92@users.noreply.github.com>
2024-01-31 14:34:28 -0700

    Added Tooltips to the portfolio items so users can know if what they should click on to visit the deployed application or the repo

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,31 @@
-export default function PortItem( props ) {
+import { Tooltip } from "react-tooltip"
+
+export default function PortItem(props) {
 
   return (
     <>
       {props.portfolioRoster.map((item) => (
         <div className="portfolio-item">
-          <a href={item.deployedLink}><img className="portfolio-item-img" src={item.image} alt={item.altTag}></img></a>
-          <a className='portfolio-item-text-area' href={item.gitLink}>
+          <a
+            className="image-link"
+            href={item.deployedLink}
+            data-tooltip-id="image-tooltip"
+            data-tooltip-content={"Click here to see the deployed application!"}
+            data-tooltip-place="top"
+          >
+
+            <img className="portfolio-item-img" src={item.image} alt={item.altTag}></img></a>
+          <Tooltip anchorSelect=".image-link" place="top" />
+          <a
+            className='portfolio-item-text-area'
+            href={item.gitLink}
+            data-tooltip-id="image-tooltip"
+            data-tooltip-content={"Click here to go to the GitHub repository!"}
+            data-tooltip-place="bottom"
+          >
             <h4>{item.itemName}</h4>
             <p className='portfolio-item-desc'> {item.description} </p></a>
+            <Tooltip anchorSelect=".portfolio-item-text-area" place="bottom" />
         </div>
       ))}
     </>
